deedb086a73659b9c707053733f2af0a883e0f31
Jeffrey Tsai <jeffrey.my.tsai@gmail.com>
2021-12-03 03:00:21 -0600

    Try again

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 #wsgi - web server gateway interrface
 from app.engine import app as application
 
-app = application.run()
-# if __name__ == "__main__":
-#     app.run()
+# app = application.run()
+if __name__ == "__main__":
+    application.run()
